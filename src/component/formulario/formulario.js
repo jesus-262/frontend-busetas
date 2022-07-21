18b9936c31f5ddc373d68e3014bad5a718a5e621
jesus <jesus-262@hotmail.com>
2022-07-21 04:11:22 -0500

    hora am pm

--- src/component/formulario/formulario.js
+++ src/component/formulario/formulario.js
@@ -240,6 +240,27 @@ function Formulario() {
       </h5>
     );
   }
+  function renderhora(hora) {
+    dayjs().format();
+    const hour = dayjs(hora).format("HH:mm:ssa");
+    // console.log(dia);
+    return (
+      <h5 style={{ padding: "5px" }}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="18"
+        height="18"
+        fill="currentColor"
+        className="bi bi-watch"
+        viewBox="0 0 16 16"
+      >
+        <path d="M8.5 5a.5.5 0 0 0-1 0v2.5H6a.5.5 0 0 0 0 1h2a.5.5 0 0 0 .5-.5V5z" />
+        <path d="M5.667 16C4.747 16 4 15.254 4 14.333v-1.86A5.985 5.985 0 0 1 2 8c0-1.777.772-3.374 2-4.472V1.667C4 .747 4.746 0 5.667 0h4.666C11.253 0 12 .746 12 1.667v1.86a5.99 5.99 0 0 1 1.918 3.48.502.502 0 0 1 .582.493v1a.5.5 0 0 1-.582.493A5.99 5.99 0 0 1 12 12.473v1.86c0 .92-.746 1.667-1.667 1.667H5.667zM13 8A5 5 0 1 0 3 8a5 5 0 0 0 10 0z" />
+      </svg>{" "}
+      {hour}
+    </h5>
+    );
+  }
 
   //deleteformulario
 
@@ -295,20 +316,7 @@ function Formulario() {
                         <div>{renderfecha(fecha)}</div>
 
                         <div>
-                          <h5 style={{ padding: "5px" }}>
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="18"
-                              height="18"
-                              fill="currentColor"
-                              className="bi bi-watch"
-                              viewBox="0 0 16 16"
-                            >
-                              <path d="M8.5 5a.5.5 0 0 0-1 0v2.5H6a.5.5 0 0 0 0 1h2a.5.5 0 0 0 .5-.5V5z" />
-                              <path d="M5.667 16C4.747 16 4 15.254 4 14.333v-1.86A5.985 5.985 0 0 1 2 8c0-1.777.772-3.374 2-4.472V1.667C4 .747 4.746 0 5.667 0h4.666C11.253 0 12 .746 12 1.667v1.86a5.99 5.99 0 0 1 1.918 3.48.502.502 0 0 1 .582.493v1a.5.5 0 0 1-.582.493A5.99 5.99 0 0 1 12 12.473v1.86c0 .92-.746 1.667-1.667 1.667H5.667zM13 8A5 5 0 1 0 3 8a5 5 0 0 0 10 0z" />
-                            </svg>{" "}
-                            {hora}
-                          </h5>
+                        {renderhora(hora)}
                         </div>
                       </div>
                       {rendereimagen(imagen_url)}
